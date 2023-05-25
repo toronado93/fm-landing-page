eaec16b0421a38b0653da5855da1bfbdd932e2c3
emin <e.ertac.p@gmail.com>
2023-05-25 12:49:48 +0100

    Navigation modified

---
+++ main.js
@@ -0,0 +1,11 @@
+const primaryHeader = document.querySelector(".primary-header");
+const mobile_nav_toggle = document.querySelector(".mobile-nav-toggle");
+const primary_navigation = document.querySelector(".primary-navigation");
+
+mobile_nav_toggle.addEventListener("click", () => {
+  primary_navigation.hasAttribute("data-visible")
+    ? mobile_nav_toggle.setAttribute("aria-expanded", false)
+    : mobile_nav_toggle.setAttribute("aria-expanded", true);
+  primary_navigation.toggleAttribute("data-visible");
+  primaryHeader.toggleAttribute("data-overlay");
+});
